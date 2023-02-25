34d55cc3b8b67cca728c3cecbc38fbc8159a8e45
Nicholas Nguyen <nick.nguyen@nyu.edu>
2023-02-25 17:33:34 -0500

    feat: add progressive web app requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      includeAssets: ['peach_128.png'],
+      manifest: {
+        name: 'Positive Doodle View',
+        short_name: 'Doodles',
+        description: 'Application for viewing, filtering, and saving positive doodles',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'peach_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "maskable any"
+          },
+          {
+            src: 'peach_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: "maskable any"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
 })
